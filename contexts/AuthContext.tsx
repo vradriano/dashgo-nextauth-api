96e3a1926e24cda3d796ce130cbd24cfaa3cbc84
unknown <vradriano@hotmail.com>
2022-06-18 08:51:32 -0300

    sending token auth, parsing cookies, seting new token after the login

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,8 +1,8 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { api } from "../services/api";
 
 import Router from 'next/router'
-import { setCookie } from 'nookies'
+import { setCookie, parseCookies } from 'nookies'
 
 
 type User = {
@@ -32,6 +32,17 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>()
   const isAuthenticated = !!user;
 
+  useEffect(() => {
+    const { 'nextauth.token': token } = parseCookies()
+
+    if (token) {
+      api.get('/me').then(response => {
+        const { email, permissions, roles } = response.data 
+
+        setUser({ email, permissions, roles})
+      })
+    }
+  }, [])
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
@@ -56,6 +67,8 @@ export function AuthProvider({ children }: AuthProviderProps) {
         maxAge: 60 * 60 * 24 * 30, // 30 days
         path: '/'
       })
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`
 
       Router.push('/dashboard')
     } catch (err) {
