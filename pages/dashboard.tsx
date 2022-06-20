6baad1a244be566be91f4c08d79862048ece2436
unknown <vradriano@hotmail.com>
2022-06-19 22:13:38 -0300

    create component to check if user has permission to see jsx in the page

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,17 +1,13 @@
 import { GetServerSidePropsContext } from "next"
 import { useContext, useEffect } from "react"
+import { Can } from "../components/Can"
 import { AuthContext } from "../contexts/AuthContext"
-import { useCan } from "../hooks/useCan"
 import { setupApiClient } from "../services/api"
 import { api } from "../services/apiclient"
 import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext)
-
-  const useCanSeeMetrics = useCan({
-    permissions: ['metrics.list']
-  })
 
   useEffect(() => {
     api.get('/me')
@@ -24,7 +20,9 @@ export default function Dashboard() {
   return (
     <>
     <h1>Dashboard {user?.email}</h1>
-    { useCanSeeMetrics && <div>Métricas</div> }
+    <Can permissions={['metrics.list']}> 
+      <div>Métricas</div>
+    </Can>
     </>
   )
 }
